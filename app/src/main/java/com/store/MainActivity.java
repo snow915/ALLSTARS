db91ac4a7143df99597287e5b9a5fc2070998560
Penetrator <penetrator@pussy.com>
2020-03-24 14:44:10 -0600

    Now the user can delete his(her) account

--- app/src/main/java/com/store/MainActivity.java
+++ app/src/main/java/com/store/MainActivity.java
@@ -1,4 +1,5 @@
 package com.store;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,6 +13,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.store.CarritoCompras.Carrito;
 import com.store.Credenciales.Login;
 import com.store.Register;
@@ -30,6 +33,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.Menu;
 import android.view.View;
@@ -72,23 +76,24 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         load_preferences();
-        if(user != null && pass != null){
-            user_id = user;
-        }
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
-        if(user_id != null){
+        if(user != null && pass != null){
+            user_id = user;
             NavigationView navView = findViewById(R.id.nav_view);
             View header = navView.getHeaderView(0);
             navView.getMenu().findItem(R.id.nav_signin).setVisible(false);
             navView.getMenu().findItem(R.id.nav_send).setVisible(true);
+            navView.getMenu().findItem(R.id.see_profile).setVisible(true);
+            navView.getMenu().findItem(R.id.edit_profile).setVisible(true);
+            navView.getMenu().findItem(R.id.delete_account).setVisible(true);
             navView.getMenu().findItem(R.id.nav_profile).setVisible(true);
         }
+        navigationView.setNavigationItemSelectedListener(this);
         carousel = new Carrusel();
         mi_fragment = new index();
         getSupportFragmentManager().beginTransaction().add(R.id.content_main, mi_fragment).commit();
@@ -238,8 +243,21 @@ public class MainActivity extends AppCompatActivity
                 mi_fragment.setArguments(b);
                 fragment_seleccionado = true;
             }
+        } else if (id == R.id.delete_account) {
+            new SweetAlertDialog( MainActivity.this, SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText("Are you sure that you want delete this?")
+                    .setContentText("This action cannot be undone")
+                    .setConfirmText("Yes")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+                            sDialog.setCanceledOnTouchOutside(false);
+                            sDialog.dismissWithAnimation();
+                            deleteUserData(user_id);
+                        }
+                    })
+                    .show();
         }
-
 
         if (fragment_seleccionado) {
             //getSupportFragmentManager().popBackStack();
@@ -252,6 +270,31 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    public void deleteUserData(String id){
+        DatabaseReference df = FirebaseDatabase.getInstance().getReference("Usuarios").child(id);
+        df.removeValue();
+        save_preferences();
+        new SweetAlertDialog( MainActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                .setTitleText("Done")
+                .setContentText("Count deleted successfully!")
+                .setConfirmText("Got it")
+                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sDialog) {
+                        sDialog.setCanceledOnTouchOutside(false);
+                        sDialog.dismissWithAnimation();
+                        reload();
+                    }
+                })
+                .show();
+    }
+
+    public void reload(){
+        Activity current_context = MainActivity.this;
+        Intent intent = new Intent(current_context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
     @Override
     public void onFragmentInteraction(Uri uri) {
 
