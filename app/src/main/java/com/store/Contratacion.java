72e77a26d172a7dd0aecf091588daa4e6b484dc4
snow915 <cris_ganor@hotmail.com>
2020-04-03 22:38:11 -0600

    add: places API but not working

--- app/src/main/java/com/store/Contratacion.java
+++ app/src/main/java/com/store/Contratacion.java
@@ -1,10 +1,12 @@
 package com.store;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,6 +16,11 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.text.DateFormat;
 import java.util.Calendar;
@@ -26,7 +33,9 @@ public class Contratacion extends AppCompatActivity implements
 
     EditText fechaInicio, fechaFin, horaInicio, horaFin, detalles;
     Spinner tipoPublico, tipoEvento;
+    Button maps;
     private int dia, mes, anio, hora, minutos;
+    int PLACE_PICKER_REQUEST = 1;
     boolean statusFechaInicio = false;
     boolean statusFechaFin = false;
     boolean statusHoraInicio = false;
@@ -43,12 +52,15 @@ public class Contratacion extends AppCompatActivity implements
         detalles = (EditText) findViewById(R.id.idDetalles);
         tipoPublico = (Spinner) findViewById(R.id.idPublico);
         tipoEvento = (Spinner) findViewById(R.id.idEvento);
+        maps = (Button) findViewById(R.id.idMaps);
 
         fechaInicio.setOnClickListener(this);
         fechaFin.setOnClickListener(this);
 
         horaInicio.setOnClickListener(this);
         horaFin.setOnClickListener(this);
+
+        maps.setOnClickListener(this);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.array_publico, android.R.layout.simple_spinner_item);
@@ -121,6 +133,35 @@ public class Contratacion extends AppCompatActivity implements
             timePicker.show(getSupportFragmentManager(), "time picker");
             statusHoraFin = true;
             statusHoraInicio = false;
+        } else if (v == maps){
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            try {
+                startActivityForResult(builder.build(Contratacion.this), PLACE_PICKER_REQUEST);
+            } catch (GooglePlayServicesRepairableException e) {
+                e.printStackTrace();
+            } catch (GooglePlayServicesNotAvailableException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, getApplicationContext());
+                StringBuilder stringBuilder = new StringBuilder();
+                String latitude = String.valueOf(place.getLatLng().latitude);
+                String longitude = String.valueOf(place.getLatLng().longitude);
+                stringBuilder.append("LATITUDE: ");
+                stringBuilder.append(latitude);
+                stringBuilder.append("\n");
+
+                stringBuilder.append("LONGITUDE: ");
+                stringBuilder.append(longitude);
+
+            }
         }
     }
 
