85f85024e1d36c175646aea82a4d7f2f27f2cd9c
snow915 <cris_ganor@hotmail.com>
2020-04-07 20:07:45 -0500

    add: map with searchview

--- app/src/main/java/com/store/Map.java
+++ app/src/main/java/com/store/Map.java
@@ -3,69 +3,72 @@ package com.store;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.SearchView;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
-import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 
-public class Map extends FragmentActivity implements OnMapReadyCallback, AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
 
-    private GoogleMap mMap;
+public class Map extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap map;
     private LatLng sydney = new LatLng(-8.579892, 116.095239);
-    private MapFragment mapFragment;
-
+    private SupportMapFragment mapFragment;
+    private SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        //mapFragment.getMapAsync(Map.this);
-        setupAutoCompleteFragment();
-    }
+        searchView = findViewById(R.id.sv_location);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
-    private void setupAutoCompleteFragment() {
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onPlaceSelected(Place place) {
-                sydney = place.getLatLng();
-                mapFragment.getMapAsync(Map.this);
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+                if (location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    map.addMarker(new MarkerOptions().position(latLng).title(location));
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng ,10));
+                }
+                return false;
             }
 
             @Override
-            public void onError(Status status) {
-                Log.e("Error", status.getStatusMessage());
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
+
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 8.5f));
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("kodetr")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        map = googleMap;
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(mMap != null) {
-            mMap.clear();
-        }
-    }
 }
