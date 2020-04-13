1258a9fa8723d1ebf4fbb63ffdf66549085a1f67
snow915 <cris_ganor@hotmail.com>
2020-04-13 13:56:30 -0500

    add: validation for hiring when te user is null

---
+++ app/src/main/java/com/store/SharedPreferences.java
@@ -0,0 +1,12 @@
+package com.store;
+
+import android.content.Context;
+public class SharedPreferences {
+
+    public String user;
+    public String password;
+
+    public SharedPreferences () {};
+    
+
+}
