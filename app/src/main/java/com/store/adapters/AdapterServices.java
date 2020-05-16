e803e9e68bd2dc4129134daff7cdb12d30f8f227
Penetrator <penetrator@pussy.com>
2020-05-15 23:37:03 -0500

    Add: fragment_view_services, view_services_item; Changes on AdapterServicios: now it works on both modal and list

--- app/src/main/java/com/store/adapters/AdapterServices.java
+++ app/src/main/java/com/store/adapters/AdapterServices.java
@@ -1,26 +1,24 @@
 package com.store.adapters;
-
 import android.content.Context;
 import android.content.Intent;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.store.InfoProducto;
 import com.store.R;
 import com.store.Servicios;
-
 import java.util.ArrayList;
 
-public class AdapterServices extends RecyclerView.Adapter<AdapterServices.ViewHolderService>{
-
+public class AdapterServices extends RecyclerView.Adapter<AdapterServices.ViewHolderService> {
     private ArrayList<String> itemNames = new ArrayList<>();
     private ArrayList<String> itemPrices = new ArrayList<>();
     private ArrayList<String> itemDescriptions = new ArrayList<>();
@@ -38,8 +36,16 @@ public class AdapterServices extends RecyclerView.Adapter<AdapterServices.ViewHo
     @NonNull
     @Override
     public ViewHolderService onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.service_item, parent, false);
-        return new ViewHolderService(view);
+        int id = 0;
+        boolean hasCardOnClickAtached = false;
+        if (String.valueOf(parent.getContext().getClass()).equals("class com.store.MainActivity")) {
+            id = R.layout.view_services_item;
+        } else {
+            id = R.layout.service_item;
+            hasCardOnClickAtached = true;
+        }
+        View view = LayoutInflater.from(parent.getContext()).inflate(id, parent, false);
+        return new ViewHolderService(view, hasCardOnClickAtached);
     }
 
     @Override
@@ -50,20 +56,22 @@ public class AdapterServices extends RecyclerView.Adapter<AdapterServices.ViewHo
         final String servicePrice = holder.servicePrice.getText().toString();
         holder.serviceDescription.setText(itemDescriptions.get(position));
         final String des = holder.serviceDescription.getText().toString();
-        holder.serviceMaximumTime.setText(itemMaximumTimes.get(position));
+        holder.serviceMaximumTime.setText(itemMaximumTimes.get(position) + " horas");
         final String maxTime = holder.serviceMaximumTime.getText().toString();
-        holder.card.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, Servicios.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("name", serviceName);
-                intent.putExtra("price", servicePrice);
-                intent.putExtra("description", des);
-                intent.putExtra("maximum_hours", maxTime);
-                context.startActivity(intent);
-            }
-        });
+        if (holder.card != null) {
+            holder.card.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, Servicios.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra("name", serviceName);
+                    intent.putExtra("price", servicePrice);
+                    intent.putExtra("description", des);
+                    intent.putExtra("maximum_hours", maxTime);
+                    context.startActivity(intent);
+                }
+            });
+        }
     }
 
 
@@ -79,16 +87,20 @@ public class AdapterServices extends RecyclerView.Adapter<AdapterServices.ViewHo
         TextView servicePrice;
         TextView serviceDescription;
         TextView serviceMaximumTime;
-        CardView card;
+        @Nullable CardView card;
 
-        public ViewHolderService(@NonNull View itemView) {
+        public ViewHolderService(@NonNull View itemView, boolean hasCardOnClickAtached) {
             super(itemView);
             serviceImage = itemView.findViewById(R.id.item_image);
             serviceName = itemView.findViewById(R.id.item_name);
             servicePrice = itemView.findViewById(R.id.item_price);
             serviceDescription = itemView.findViewById(R.id.item_description);
             serviceMaximumTime = itemView.findViewById(R.id.item_maximum_time);
-            card = itemView.findViewById(R.id.item_card);
+            if (hasCardOnClickAtached) {
+                card = itemView.findViewById(R.id.item_card);
+            } else {
+                card = null;
+            }
         }
     }
 }
---
+++ app/src/main/java/com/store/famous/FragmentViewServices.java
@@ -0,0 +1,120 @@
+package com.store.famous;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.store.InfoProducto;
+import com.store.R;
+import com.store.SharedPreferencesApp;
+import com.store.adapters.AdapterDatos;
+import com.store.adapters.AdapterServices;
+import com.store.vo.DatosVo;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentViewServices#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentViewServices extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+    private RecyclerView recycler;
+    private DatabaseReference reference;
+    private SharedPreferencesApp preferencesApp;
+    private String artistUserName;
+    private TextView description;
+    public FragmentViewServices() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentViewServices.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentViewServices newInstance(String param1, String param2) {
+        FragmentViewServices fragment = new FragmentViewServices();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_view_services, container, false);
+        fillData(view);
+        return view;
+    }
+
+    private void fillData(View v){
+        final View view = v;
+        final ArrayList<String> itemNames = new ArrayList<>();
+        final ArrayList<String> itemPrices = new ArrayList<>();
+        final ArrayList<String> itemDescriptions = new ArrayList<>();
+        final ArrayList<String> itemMaximumTimes = new ArrayList<>();
+        preferencesApp = new SharedPreferencesApp(getContext());
+        preferencesApp.loadPreferences();
+        artistUserName = preferencesApp.getArtistUsername();
+        reference = FirebaseDatabase.getInstance().getReference().child("data").child(artistUserName).child("servicios");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot element : dataSnapshot.getChildren()) {
+                    itemNames.add(element.child("nombre").getValue().toString());
+                    itemPrices.add(element.child("precio").getValue().toString());
+                    itemDescriptions.add(element.child("detalles").getValue().toString());
+                    itemMaximumTimes.add(element.child("tiempoMaximo").getValue().toString());
+                }
+                recycler = view.findViewById(R.id.recycler_list_services);
+                recycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+                AdapterServices adapter = new AdapterServices(itemNames, itemPrices, itemDescriptions, itemMaximumTimes, getContext());
+                recycler.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
